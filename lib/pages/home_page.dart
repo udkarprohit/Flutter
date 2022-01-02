3a8bf918ca9f9a5c62b661e3f19e5c6d4af60af8
Prohit <udkarprohit@gmail.com>
2022-01-02 20:09:13 +0530

    day11 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,21 +13,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => Catalogmodel.item[0]);
+
     return Scaffold(
-        appBar: AppBar(
-          title:  Text("Catalog", 
-          style: TextStyle(
-            color: Colors.black,
-          ))
-        ),
+      appBar: AppBar(
+          title: Text("Catalog",
+              style: TextStyle(
+                color: Colors.black,
+              ))),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(
-        itemCount: Catalogmodel.item.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(item: Catalogmodel.item[index],);
-        },
-      ),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: MyDrawer(),
     );
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -11,11 +11,21 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(item.image),
-      title: Text(item.name),
-      subtitle: Text(item.desc),
-      trailing: Text(item.price.toString()),
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} pressed");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}",
+        textScaleFactor: 1.5,
+        style: TextStyle(
+          color: Colors.deepPurple,
+          fontWeight: FontWeight.bold,
+        ),
+      )),
     );
   }
 }
