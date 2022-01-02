36033cda64aacc02a431fb2d78aa9b41d899ec6c
Prohit <udkarprohit@gmail.com>
2022-01-02 19:46:01 +0530

    day10 completed

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,18 @@ import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
-import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
+
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
         
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,33 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Bittubhai";
 
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:  Text("Catalog App"),
+          title:  Text("Catalog", 
+          style: TextStyle(
+            color: Colors.black,
+          ))
         ),
-      body: Center(
-        child: Container(
-          child: Text(
-              "Welcome to $days FlutterApp by $name"), //$day repesents interpellations if there r more than 1 words then we have to use {}
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+      child: ListView.builder(
+        itemCount: Catalogmodel.item.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(item: Catalogmodel.item[index],);
+        },
+      ),
       ),
       drawer: MyDrawer(),
     );
